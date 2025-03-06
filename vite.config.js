6e20e86d17ba7a3d803afdd49ce6cddc7e0ee017
nghiatk54 <nghiatk54@gmail.com>
2025-03-07 06:08:55 +0700

    base structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+const __dirname = path.resolve();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@component": path.resolve(__dirname, "src/component"),
+      "@style": path.resolve(__dirname, "src/assets/style"),
+    },
+  },
+});
